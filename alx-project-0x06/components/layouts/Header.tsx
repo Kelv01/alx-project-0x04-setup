185e59772ff20ebd178f9060fa6cfd697d769b29
kelv01 <kelvinnjugunanungari@gmail.com>
2025-11-30 13:25:46 +0300

    redux  mapping update

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -8,7 +8,6 @@
 //   const pathname = usePathname()
 //   const {count} = useCount()
 
-
 //   return (
 //     <header className="fixed w-full bg-white shadow-md">
 //       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
@@ -32,7 +31,7 @@
 //           ) : (
 //             <p className="font-semibold text-lg">Current count : {count} </p>
 //           ) }
-          
+
 //         </div>
 //       </div>
 //     </header>
@@ -40,7 +39,6 @@
 // };
 
 // export default Header;
-
 
 //update to use redux
 
@@ -51,36 +49,29 @@ import { RootState } from "@/store/store";
 import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
-
-  const pathname = usePathname()
-  const count = useSelector((state: RootState) => state.counter.value)
-
+  const pathname = usePathname();
+  const count = useSelector((state: RootState) => state.counter.value);
 
   return (
     <header className="fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
-        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+        <Link
+          href="/"
+          className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight"
+        >
           Splash App
         </Link>
 
         {/* Button Group */}
         <div className="flex gap-4">
-          {
-            !["/counter-app"].includes(pathname) ? (
-              <>
-              <Button
-            buttonLabel="Sign In"
-            buttonBackgroundColor="red"
-          />
-          <Button
-            buttonLabel="Sign Up"
-            buttonBackgroundColor="blue"
-          /></>
-            ) : (
-              <p className=" font-semibold text-lg">Current count : {count}</p>
-            )
-          }
-
+          {!["/counter-app"].includes(pathname) ? (
+            <>
+              <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
+              <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
+            </>
+          ) : (
+            <p className=" font-semibold text-lg">Current count : {count}</p>
+          )}
         </div>
       </div>
     </header>
